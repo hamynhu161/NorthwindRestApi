0406bd67a84cb4a39675f61724d0dbef8633d6c9
Nhu Ha <hamynhu161@gmail.com>
2025-05-03 20:03:11 +0300

    Käyttäjille lisätty poisto- ja muokkaustoiminto

--- NorthwindRestApi/Controllers/UsersController.cs
+++ NorthwindRestApi/Controllers/UsersController.cs
@@ -42,6 +42,50 @@ namespace NorthwindRestApi.Controllers
                 return BadRequest("lisääminen ei onnistunut. Tässä lisätietoa: " + ex);
             }
         }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var user = db.Users.Find(id);
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                    return Ok("User " + user.Firstname + " poistettiin.");
+                }
+                return NotFound($"Käyttäjää id:llä {id} ei löytynyt.");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult EditUser(int id, [FromBody] User user)
+        {
+            try
+            {
+                var käyttäjä = db.Users.Find(id);
+                if (käyttäjä != null)
+                {
+                    käyttäjä.Firstname = user.Firstname;
+                    käyttäjä.Lastname = user.Lastname;
+                    käyttäjä.Email = user.Email;
+                    käyttäjä.AccesslevelId = user.AccesslevelId;
+
+                    db.SaveChanges();
+                    return Ok("Muokattu käyttäjää " + user.Firstname);
+                }
+                return NotFound("Käyttäjä ei löytynyt id:llä " + id);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
     }
 
     
